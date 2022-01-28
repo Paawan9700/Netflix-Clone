4b82ea2459fc895c210ecad39102eee70c86c1d1
Paawan Singal <paawansingal09@gmail.com>
2022-01-28 16:06:06 +0530

    banner build

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -24,6 +24,7 @@ export const Banner = () => {
     // console.log("abc");
     // console.log(movie);
 
+    // a standard function just to truncate the paragraph or something like that
     function truncate(str, n) {
         return str?.length > n ? str.substr(0, n - 1) + "..." : str;
     }
@@ -33,11 +34,15 @@ export const Banner = () => {
             style={{
                 backgroundSize: "cover",
                 backgroundPosition: "center center",
+                // here question mark actually means that if somehow the required movie is not there then
+                // app will not gonna crash and handle somehow
                 backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             }}>
 
             <div className="banner_contents">
                 <h1 className="banner_title">
+                    {/* some api actually does not give consistent information so movie's name can be present in different format */}
+                    {/* // this is the way to deal that */}
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
 
@@ -51,6 +56,7 @@ export const Banner = () => {
                 </h1>
             </div>
 
+            {/* just to have that faded effect at the bottom */}
             <div className="banner--fadeBottom" />
 
         </header>
